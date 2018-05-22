1124f80d93093f318b5ef3d86dc0179d42506d48
Meghan Debity <mdebity@gmail.com>
2018-05-22 13:40:00 -0500

    writing to DOM and all elements are being colled from json correctly

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
+// const $ = require("jquery")
 $(document).ready(function() {
     // Use jQuery to get a reference to `load-songs`
     const loadSongsBtn = $("#load-songs")
-    // Use jQuery to get a reference to `song-list`
-    const songList = $("#song-list")
     //Use ajax to load songs.json
     loadSongsBtn.on("click", function() {
         $.ajax("http://localhost:8088/songs").then(allSongs => {
-            console.log(allSongs.forEach(
-                currentSection => {
+            allSongs.forEach(currentSong => {
+                // Use jQuery to get a reference to `song-list`
+                    const songList = $("#song-list")
+
                     const songSection = document.createElement("section")
                     songSection.setAttribute("class", "song")
+                    songList.append(songSection)
+
+                    const songHeader = document.createElement("h1")
+                    songHeader.textContent = currentSong.title
+                    songHeader.setAttribute("class", "song__title")
+
+                    songSection.append(songHeader)
+
+                    const songDescriptionSection = document.createElement("section")
+                    songDescriptionSection.setAttribute("class", "song__description")
+                    songDescriptionSection.textContent = "Performed by " + `${currentSong.artist}` + " on the album " + `${currentSong.album}`
+                    songSection.append(songDescriptionSection)
                 }
-            ))
+            )
         })
     })
-
-    /*
-        Chain a `.then()` method to the ajax call, and when
-        it is complete build a DOM component for each song with
-        the following structure. Use the jQuery append() method
-        to put an HTML representation of each song the DOM as a
-        child component of the .
-
-            <section class="song">
-                <h1 class="song__title">{Title of song}</h1>
-                <section class="song__description">
-                    Performed by {artist} on the album {album}
-                </section>
-            </section>
-    */
 })
