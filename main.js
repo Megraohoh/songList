f2c13e935b16ff2b3dd93de1f4d7fd2245418975
Meghan Debity <mdebity@gmail.com>
2018-05-22 11:46:14 -0500

    updated ajax methods, starting html elements build

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-//Practice jQuery
-$(document).ready(function () {
-    const btnLoadSongs = $('#load-songs')
-    const divSongList = $('#song-list')
-
-    btnLoadSongs.click(function () {
-        $.ajax({ url: "songs.json", method: "GET" })
-            .then( //when json is loaded...response is in relation to the loaded json
-                response => response.songs.forEach(song => divSongList.append(
-                    `    
-                <section class="song">
-                    <h1 class="song__title">${song.title}</h1>
-                    <section class="song__description">
-                    Performed by ${song.artist} on the album ${song.album}</section>
-                </section>
-                `
-                ))
-            )
+$(document).ready(function() {
+    // Use jQuery to get a reference to `load-songs`
+    const loadSongsBtn = $("#load-songs")
+    // Use jQuery to get a reference to `song-list`
+    const songList = $("#song-list")
+    //Use ajax to load songs.json
+    loadSongsBtn.on("click", function() {
+        $.ajax("http://localhost:8088/songs").then(allSongs => {
+            console.log(allSongs.forEach(
+                currentSection => {
+                    const songSection = document.createElement("section")
+                    songSection.setAttribute("class", "song")
+                }
+            ))
+        })
     })
-})
+
     /*
-    Chain a `.then()` method to the ajax call, and when
-    it is complete build a DOM component for each song with
-    the following structure. Use the jQuery append() method
-    to put an HTML representation of each song the DOM as a
-    child component of the .
-    
-    <section class="song">
-    <h1 class="song__title">{Title of song}</h1>
-    <section class="song__description">
-    Performed by {artist} on the album {album}
-    </section>
-    </section>
+        Chain a `.then()` method to the ajax call, and when
+        it is complete build a DOM component for each song with
+        the following structure. Use the jQuery append() method
+        to put an HTML representation of each song the DOM as a
+        child component of the .
+
+            <section class="song">
+                <h1 class="song__title">{Title of song}</h1>
+                <section class="song__description">
+                    Performed by {artist} on the album {album}
+                </section>
+            </section>
     */
+})
